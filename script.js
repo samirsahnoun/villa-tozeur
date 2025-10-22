51c8160a4edf3d97c3e91c77c70228a282098153
samirgit <samir.sahnoun@gmail.com>
2025-10-22 11:27:18 +0100

    Mise a jour automatique du site (22/10/2025 11:27:18,26)

--- script.js
+++ script.js
@@ -158,4 +158,5 @@
     const yearEl = document.getElementById('year');
     if (yearEl) yearEl.textContent = new Date().getFullYear();
   });
-})();
+})();// juste après: const lightbox = $('#lightbox');
+lightbox.style.touchAction = 'pan-y'; // évite les conflits swipe/horizontal
